aba0a40a54eb8d894c99c5f6fe049759c2f72de9
asiaromaria <designsbyarc@gmail.com>
2021-07-12 11:49:47 -0700

    updated player.js by adding module.exports

--- Player.js
+++ Player.js
@@ -1,7 +1,11 @@
-class Player {
+
+class Player{
     constructor(){
         this.score = 0
         this.choice;
         this.handGame = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
     }
+
 }
+
+module.exports = user;
